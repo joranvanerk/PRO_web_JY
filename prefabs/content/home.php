eee7e0ca34425349e0ef98be586c85745d20f7c9
yusuf <321611@student.mboutrecht.nl>
2021-02-17 13:36:51 +0100

    update home.php
    comments toegevoegd

--- prefabs/content/home.php
+++ prefabs/content/home.php
@@ -1,17 +1,24 @@
 <section class="fdb-block">
+<!-- begin van container -->
   <div class="container">
+  <!-- begin van row -->
     <div class="row align-items-center pb-xl-5">
       <div class="col-12 col-md-7 col-xl-5">
+      <!-- titel tekst -->
         <h1>SpaceY</h1>
+        <!-- banner tekst -->
         <p class="lead"> Welkom op de site van SpaceY. Hier zul je informatie vinden over de opleiding. Je kunt hier een spel vinden dat jou een beeld zal geven over de opleiding. Veel succes!</p>
       </div>
       <div class="col-12 col-sm-6 col-md-4 m-sm-auto mr-md-0 ml-md-auto pt-4 pt-md-0">
+      <!-- geanimeerde lottie file -->
       <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_XWP6mV.json"  background="transparent"  speed="1"  style="width: 400px; height: 400px;"  loop  autoplay></lottie-player>
       </div>
     </div>
-
+    
+    <!-- begin van row -->
     <div class="row pt-5">
+    <!-- responsive maken -->
       <div class="col-12 col-sm-6 col-md-3">
       <!-- prijs feature -->
         <h3><strong>Prijs</strong></h3>
@@ -19,13 +26,15 @@
         <p>Wij hebben de beste prijzen voor de Kwaliteit dat je gaat krijgen bij ons.</p>
       </div>
 
+      <!-- responsive maken -->
       <div class="col-12 col-sm-6 col-md-3 pt-3 pt-sm-0">
       <!-- kwaliteit feature -->
         <h3><strong>Kwaliteit</strong></h3>
         <!-- tekst van kwaliteit -->
         <p>Wij hebben de beste leer methode opgezet voor de studenten voor het uiterste resultaat.</p>
       </div>
-
+   
+      <!-- responsive maken -->
       <div class="col-12 col-sm-6 col-md-3 pt-3 pt-md-0">
       <!-- interaactief feature -->
         <h3><strong>Interactief</strong></h3>
@@ -33,6 +42,7 @@
         <p>Er is een spel voor jullie opgezet om een beeld te krijgen van de opleiding.</p>
       </div>
 
+      <!-- responsive maken -->
       <div class="col-12 col-sm-6 col-md-3 pt-3 pt-md-0">
       <!-- praktijk feature -->
         <h3><strong>Praktijk</strong></h3>
@@ -41,4 +51,5 @@
       </div>
     </div>
   </div>
+  <!-- einde sectie -->
 </section>
