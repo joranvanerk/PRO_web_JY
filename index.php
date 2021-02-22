8b8be0f37f3a7865b57490fbafcb971b1a6dc8de
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-02-22 14:10:57 +0100

    Update index.php
    Added preloader and background gradients

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <html lang="en">
   <head>
+
+    <style>
+
+
+
+    </style>
     <!-- alle meta tags -->
     <?php include_once("./core/required_meta_tags.html") ?>
 
     <!-- Alle style -->
     <?php include_once("./core/style.html") ?>
+    <link rel="stylesheet" href="./core/css/main-core.css">
     <!-- Titel of deze website -->
-    <title>echo titel hier!!!!</title>
+    <title>SPACEY</title>
     
   </head>
     <!-- Background -->
@@ -17,12 +24,27 @@
 
     <!-- content van de pagina :) ;) :D-->
     <?php include_once("./prefabs/content/content.php") ?> 
-    
+    <script src="jquery-2.1.4.js"></script>
+    <div class="loader-wrapper">
+    <div class="preload">
+    <div class="preload-status">
+        <div class="preload-status-bar"></div>
+        <div class="preload-status-info">SPACEY</div>
+    </div>
+</div>
+  </span>
+    </div>
     <!-- Is dat tekst onder te website die je ziet -->
     <footer class="page-footer center-on-small-only pt-0 mt-5"><?php include_once("./prefabs/footer/footer.php") ?></footer>
-
     <!-- Alle javascript en script bestanden -->
     <?php include_once("./core/script.html"); ?>
     
+
+    <script>
+        $(window).on("load",function(){
+          $(".loader-wrapper").fadeOut("slow");
+        });
+    </script>
+    <link rel="script" href="./core/script.js" />
   </body>
 </html>
