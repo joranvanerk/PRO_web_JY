272d7c9c10bbdd57d4b7712b2865df05f39edade
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-03-13 19:44:56 +0100

    Update home.php
    changed the core and how the page loads for performance issues and added the PRO prefix

--- paneel/home.php
+++ paneel/home.php
@@ -8,12 +8,18 @@ require PATH_LIB . "page-top.php";
 ?>
 
 
-<?php include_once("./connect_db.php");
+<?php 
 
-    $sql = "SELECT * FROM `home`";
+    // de database connectie opzette
+    include_once("./connect_db.php");
 
+    // de query invoeren
+    $sql = "SELECT * FROM `pro_home`";
+
+    // de query uitvoeren
     $result = mysqli_query($conn, $sql);
 
+    // de resultaten pakken
     $record = mysqli_fetch_assoc($result);
     
 ?>
@@ -26,21 +32,28 @@ require PATH_LIB . "page-top.php";
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   </head>
+  <!-- het begin van de body -->
   <body>
+  <!-- een snelle container-fluid -->
   <div class="container-fluid">
+  <!-- de standaard container -->
   <main class="container">
     </div>
         <div class="col6">
+          <!-- een formulier voor makkelijk versturen -->
           <form action="" method="POST">
             <div class="form-group">
+              <!-- label -->
               <label for="titel">Titel</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="titel" class="form-control" id="titel" aria-describedby="firstnameHelp" value="<?php echo $record["titel"]; ?>">
             </div>
             <div class="form-group">
+            <!-- label -->
               <label for="tekst">Heading</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="heading" class="form-control" id="heading" aria-describedby="lastnameHelp" value="<?php echo $record["heading"]; ?>">
             </div>
             <input type="submit" name="submit" value="Opslaan">
@@ -48,13 +61,18 @@ require PATH_LIB . "page-top.php";
           </form>
           <br>
 
+          <!-- een formulier voor makkelijk versturen -->
           <form action="" method="POST">
             <div class="form-group">
+            <!-- label -->
               <label for="titel">Card 1</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card1" class="form-control" id="card1" aria-describedby="firstnameHelp" value="<?php echo $record["card1"]; ?>">
             </div>
             <div class="form-group">
+            <!-- label -->
               <label for="tekst">Card 1 tekst</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card1tekst" class="form-control" id="card1tekst" aria-describedby="lastnameHelp" value="<?php echo $record["card1tekst"]; ?>">
             </div>
             <input type="submit" name="submit1" value="Opslaan">
@@ -62,13 +80,18 @@ require PATH_LIB . "page-top.php";
           </form>
           <br>
 
+          <!-- een formulier voor makkelijk versturen -->
           <form action="" method="POST">
             <div class="form-group">
+            <!-- label -->
               <label for="titel">Card 2</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card2" class="form-control" id="card2" aria-describedby="firstnameHelp" value="<?php echo $record["card2"]; ?>">
             </div>
             <div class="form-group">
+            <!-- label -->
               <label for="tekst">Card 2 tekst</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card2tekst" class="form-control" id="card2tekst" aria-describedby="lastnameHelp" value="<?php echo $record["card2tekst"]; ?>">
             </div>
             <input type="submit" name="submit2" value="Opslaan">
@@ -76,13 +99,18 @@ require PATH_LIB . "page-top.php";
           </form>
           <br>
 
+          <!-- een formulier voor makkelijk versturen -->
           <form action="" method="POST">
             <div class="form-group">
+            <!-- label -->
               <label for="titel">Card 3</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card3" class="form-control" id="card3" aria-describedby="firstnameHelp" value="<?php echo $record["card3"]; ?>">
             </div>
             <div class="form-group">
+            <!-- label -->
               <label for="tekst">Card 3 tekst</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card3tekst" class="form-control" id="card3tekst" aria-describedby="lastnameHelp" value="<?php echo $record["card3tekst"]; ?>">
             </div>
             <input type="submit" name="submit3" value="Opslaan">
@@ -90,13 +118,18 @@ require PATH_LIB . "page-top.php";
           </form>
           <br>
 
+          <!-- een formulier voor makkelijk versturen -->
           <form action="" method="POST">
             <div class="form-group">
+            <!-- label -->
               <label for="titel">Card 4</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card4" class="form-control" id="card4" aria-describedby="firstnameHelp" value="<?php echo $record["card4"]; ?>">
             </div>
             <div class="form-group">
+            <!-- label -->
               <label for="tekst">Card 4 tekst</label>
+              <!-- het record en de input aanpassen -->
               <input type="text" name="card4tekst" class="form-control" id="card4tekst" aria-describedby="lastnameHelp" value="<?php echo $record["card4tekst"]; ?>">
             </div>
             <input type="submit" name="submit4" value="Opslaan">
@@ -128,8 +161,8 @@ require PATH_LIB . "page-top.php";
 </html>
 
 <?php
-    require('connect_db.php');
 
+    // alle variabelen uit de postwaarden binnenhalen
     $titel = $_POST["titel"];
     $heading = $_POST["heading"];
     $card1 = $_POST["card1"];
@@ -141,42 +174,67 @@ require PATH_LIB . "page-top.php";
     $card4 = $_POST["card4"];
     $card4tekst = $_POST["card4tekst"];
 
+    // kijken naar een submit
     if(isset($_POST["submit"])){
-      if($query = mysqli_query($connect,"UPDATE home SET titel='$titel', heading='$heading'")){
-          echo "Opgeslagen (klik <a href='./home.php'>hier</a> om te herladen) <br>";
+      // als de query er is dan uitvoeren
+      if($query = mysqli_query($connect,"UPDATE pro_home SET titel='$titel', heading='$heading'")){
+        // melden dat het is opgeslagen en refreshen
+          echo "Opgeslagen <br>";
+          echo '<meta http-equiv="refresh" content="0; url=home">';
       }else{
+          // en anders melden dat er een fout is
           echo "Fout" . mysqli_error($connect);
       }
   }
 
+  // kijken naar een submit1
   if(isset($_POST["submit1"])){
-    if($query = mysqli_query($connect,"UPDATE home SET card1='$card1', card1tekst='$card1tekst'")){
-        echo "Opgeslagen (klik <a href='./home.php'>hier</a> om te herladen) <br>";
+    // als de query er is dan uitvoeren
+    if($query = mysqli_query($connect,"UPDATE pro_home SET card1='$card1', card1tekst='$card1tekst'")){
+      // melden dat het is opgeslagen en refreshen
+        echo "Opgeslagen <br>";
+        echo '<meta http-equiv="refresh" content="0; url=home">';
     }else{
+        // en anders melden dat er een fout is
         echo "Fout" . mysqli_error($connect);
     }
 }
 
+// kijken naar een submit2
 if(isset($_POST["submit2"])){
+  // als de query er is dan uitvoeren
   if($query = mysqli_query($connect,"UPDATE home SET card2='$card2', card2tekst='$card2tekst'")){
-      echo "Opgeslagen (klik <a href='./home.php'>hier</a> om te herladen) <br>";
+    // melden dat het is opgeslagen en refreshen
+      echo "Opgeslagen <br>";
+      echo '<meta http-equiv="refresh" content="0; url=home">';
   }else{
+      // en anders melden dat er een fout is
       echo "Fout" . mysqli_error($connect);
   }
 }
 
+// kijken naar een submit3
 if(isset($_POST["submit3"])){
-  if($query = mysqli_query($connect,"UPDATE home SET card3='$card3', card3tekst='$card3tekst'")){
-      echo "Opgeslagen (klik <a href='./home.php'>hier</a> om te herladen) <br>";
+  // als de query er is dan uitvoeren
+  if($query = mysqli_query($connect,"UPDATE pro_home SET card3='$card3', card3tekst='$card3tekst'")){
+    // melden dat het is opgeslagen en refreshen
+      echo "Opgeslagen <br>";
+      echo '<meta http-equiv="refresh" content="0; url=home">';
   }else{
+      // en anders melden dat er een fout is
       echo "Fout" . mysqli_error($connect);
   }
 }
 
+// kijken naar een submit4
 if(isset($_POST["submit4"])){
-  if($query = mysqli_query($connect,"UPDATE home SET card4='$card4', card4tekst='$card4tekst'")){
-      echo "Opgeslagen (klik <a href='./home.php'>hier</a> om te herladen) <br>";
+  // als de query er is dan uitvoeren
+  if($query = mysqli_query($connect,"UPDATE pro_home SET card4='$card4', card4tekst='$card4tekst'")){
+      // melden dat het is opgeslagen en refreshen
+      echo "Opgeslagen <br>";
+      echo '<meta http-equiv="refresh" content="0; url=home">';
   }else{
+      // en anders melden dat er een fout is
       echo "Fout" . mysqli_error($connect);
   }
 }
